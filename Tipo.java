534d5311f349133dcdd3cdf90e77f3c7e5421e5e
miguel1606-creator <miguel.marangoni@aluno.ifsp.edu.br>
2025-10-29 19:43:44 -0300

    Create Tipo class with descrição property

---
+++ Tipo.java
@@ -0,0 +1,9 @@
+public class Tipo {
+    private String descrição;
+    public String getDescrição() {
+        return descrição;
+    }
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+}
